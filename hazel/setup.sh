1c5e6d30aa53a88a3248bbfae40a195f9fbdf25f
illuminati-rv <ric-v@outlook.com>
2018-02-03 22:42:45 +0530

    setup updates

--- hazel/setup.sh
+++ hazel/setup.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 mkdir nltk_data
+mkdir nltk_data/tokenizers
+mkdir nltk_data/corpora
 
-unzip -d nltk_data/nltk_data/ -o nltk_data/punkt.zip
-unzip -d nltk_data/nltk_data/ -o nltk_data/stopwords.zip
+unzip -d nltk_data/nltk_data/tokenizers/ -o nltk_data/punkt.zip
+unzip -d nltk_data/nltk_data/corpora/ -o nltk_data/stopwords.zip
 
-rm nltk_data/punkt.zip
-rm nltk_data/stopwords.zip
+rm -rf nltk_data/
 
 mv nltk_data/nltk_data/ ~/
