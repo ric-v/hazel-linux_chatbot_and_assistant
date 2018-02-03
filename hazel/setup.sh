d4d159579af3bc2f2c341428c264465d448139e3
illuminati-rv <ric-v@outlook.com>
2018-02-03 22:31:30 +0530

    update

--- hazel/setup.sh
+++ hazel/setup.sh
@@ -5,7 +5,7 @@ mkdir nltk_data
 unzip -d nltk_data/nltk_data/ -o nltk_data/punkt.zip
 unzip -d nltk_data/nltk_data/ -o nltk_data/stopwords.zip
 
-rm punkt.zip
-rm stopwords.zip
+rm nltk_data/punkt.zip
+rm nltk_data/stopwords.zip
 
-mv nltk_data/ ~/
+mv nltk_data/nltk_data/ ~/
